b8a3ff2d8f4c8912fb559d4ffffcf915c4c4db41
kizelbasheva19 <kizelbasheva19@gmail.com>
2021-10-18 03:22:43 -0400

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'scottjs/wp-dotenv' => 
+    array (
+      'pretty_version' => '1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1386db5fa27dbedc17180e3e4de36b6974bad097',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'vlucas/phpdotenv' => 
+    array (
+      'pretty_version' => 'v2.6.7',
+      'version' => '2.6.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b786088918a884258c9e3e27405c6a4cf2ee246e',
+    ),
+  ),
+);
